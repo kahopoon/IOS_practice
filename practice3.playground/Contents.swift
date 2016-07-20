001c078bb206dfaed0ca3dcea3fe813e905c1e31
jchobanana <jchobanana@gmail.com>
2016-07-21 01:05:32 +0800

    Q1-Q3

--- practice3.playground/Contents.swift
+++ practice3.playground/Contents.swift
@@ -1,1 +1,36 @@
 // HW3 iOS_practice
+
+
+// Q1 Total
+
+var sum = 0
+
+for i in 0...7{
+    for j in 0...7{
+        sum += i*j
+    }
+}
+sum
+
+
+// Q2  Odd # Lines
+
+var yo = 0
+
+for j in 0...7{
+    for i in 0...7 where i % 2 == 1{
+        yo += i*j
+    }
+}
+yo
+
+
+// Q3 Triangle Shape 
+
+var mrTrinagle = 0
+for i in 0...7{
+    for j in 0...7 where j <= i-1{
+        mrTrinagle += i*j
+    }
+}
+mrTrinagle

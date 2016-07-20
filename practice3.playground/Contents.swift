b6973580a2838ccb9c723993d8368edb9fef36ee
jchobanana <jchobanana@gmail.com>
2016-07-21 01:08:43 +0800

    all Qs listed

--- practice3.playground/Contents.swift
+++ practice3.playground/Contents.swift
@@ -34,3 +34,27 @@ for i in 0...7{
     }
 }
 mrTrinagle
+
+
+
+
+// 4_定義function，接受3個參數，起始值，最大值和決定數字倍數的number， 回傳運算結果  比方起始值3，最大值98，決定數字倍數的number為5時，(只包含5的倍數)  運算結果為 5 + 10 + 15 + ..... + 95
+
+
+// 5_定義function，接受3個參數，起始值，最大值和決定數字倍數的number， 回傳運算結果  比方起始值3，最大值11，決定數字倍數的number為5時，(不包含5的倍數)  運算結果為 3 + 4 + 6 + 7 + 8 + 9 + 11
+
+
+
+// 6_奇數行的數字總合, 定義function，接受2個參數，分別代表行數和列數
+
+
+
+// 7_所有格子的總合，除了列數>=行數的格子, 定義function，接受2個參數，分別代表行數和列數
+
+
+
+// 8_定義一個function，接受一個參數代表華式溫度 ， 回到攝式溫度
+
+
+
+// 9_定義 a function that takes an array of integers and sorts them into arrays for even and odd integers use tuple to return even array & odd array

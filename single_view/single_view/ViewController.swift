a46c9581e2f9f1dfeb9078d3393be6ac12d065c9
jchobanana <jchobanana@gmail.com>
2016-07-22 14:36:58 +0800

    first commit_singleView

---
+++ single_view/single_view/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  single_view
+//
+//  Created by TingChun Cho on 2016/7/22.
+//  Copyright © 2016年 TingChun Cho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

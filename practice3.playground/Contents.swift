2cffc9d4a03039aa6e053652694a82c0bf33d8b1
jchobanana <jchobanana@gmail.com>
2016-08-03 22:04:06 +0800

    Q5

--- practice3.playground/Contents.swift
+++ practice3.playground/Contents.swift
@@ -55,9 +55,21 @@ func ThreeNumbers(min:Int, max:Int, times:Int) ->Int {
 ThreeNumbers(min: 3, max: 98, times: 5)
 
 
-
-
 // 5_定義function，接受3個參數，起始值，最大值和決定數字倍數的number， 回傳運算結果  比方起始值3，最大值11，決定數字倍數的number為5時，(不包含5的倍數)  運算結果為 3 + 4 + 6 + 7 + 8 + 9 + 11
+
+func QFive(min:Int, max:Int, special:Int) ->Int {
+    var sum = 0
+    if min < max {
+        for i in min...max {
+            if i != special {
+                sum += i
+            }
+        }
+    }
+    return sum
+}
+
+QFive(min: 3, max: 11, special: 5)
 
 
 
